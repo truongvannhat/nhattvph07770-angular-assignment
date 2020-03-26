659447e4dfd49e5e6d71fa63a5eaf89f3cef85af
truongvannhat <truongvannhat200x@gmail.com>
2020-03-26 22:08:17 +0700

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ListPostsComponent } from './list-posts/list-posts.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ListPostsComponent, NavComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
