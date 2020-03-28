816e270029499f3ecb13718f661a413d911b99e3
truongvannhat <truongvannhat200x@gmail.com>
2020-03-28 22:59:13 +0700

    1 file modified

--- src/product.service.ts
+++ src/product.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Product } from './Product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProductService {
+  api = 'https://5c999ea71a4c360014a0ff5b.mockapi.io/products';
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.api);
+  }
+  getProduct(id): Observable<Product>{
+    return this.http.get<Product>(`${this.api}/${id}`);
+    // return this.products.find(product => product.id == id);
+  }
+  removeProduct(id){
 
+  }
+  addProduct(product){
+    // const newProduct = { id: 5, ...product};
+    // this.products.push(newProduct);
+    // console.log(this.products)
+  }
+  updateProduct(product){
+     return this.http.put<Product>(`${this.api}/${product.id}`, product);
+  }
 }
