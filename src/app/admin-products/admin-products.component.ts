8923a961eec9d5c24ce94a876bea4ce3014d0c6a
truongvannhat <truongvannhat200x@gmail.com>
2020-03-28 23:01:17 +0700

    1 file modified

--- src/app/admin-products/admin-products.component.ts
+++ src/app/admin-products/admin-products.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
+import { Product } from '../Product';
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+
+  constructor(
+    private productService: ProductService
+  ) {
+    console.log('constructor')
+   }
 
   ngOnInit() {
+    this.getProducts();
   }
-
+  getProducts(){
+    this.products = this.productService.getProducts();
+  }
+   removeItem(id){
+    // this.productService.removeProduct(id);
+    this.products = this.products.filter(product => product.id != id);
+  }
 }
