e3437ae931845e21713dea1bb5d7de75340769cf
truongvannhat <truongvannhat200x@gmail.com>
2020-03-28 18:53:26 +0700

    3 files modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
+import { FeaturesComponent } from './features/features.component';
+import { ProductsComponent } from './products/products.component';
+import { CategoryComponent } from './category/category.component';
+import { DealComponent } from './deal/deal.component';
+import { BrandComponent } from './brand/brand.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     BannerComponent,
+    FeaturesComponent,
+    ProductsComponent,
+    CategoryComponent,
+    DealComponent,
+    BrandComponent,
   ],
   imports: [
     BrowserModule,
