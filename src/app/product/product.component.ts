cf4fae99521ed2b3fd29bdac5f06021493073776
truongvannhat <truongvannhat200x@gmail.com>
2020-03-29 14:39:40 +0700

    3 files modified

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../Product';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+ product: Product;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this.getProduct();
+  }
+
+  getProduct(){
+    this.route.params.subscribe(param => {
+        this.productService.getProduct(param.id).subscribe(data => this.product = data);
+    })
   }
 
 }
